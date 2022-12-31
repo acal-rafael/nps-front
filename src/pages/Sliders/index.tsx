05baa6a453357f529ae9394877d2f8f69cab9e7b
Rafael Santos <rafaelsantos@acalhomecenter.com.br>
2022-12-31 08:42:18 -0300

    test render/redirect components

--- src/pages/Sliders/index.tsx
+++ src/pages/Sliders/index.tsx
@@ -6,40 +6,23 @@ import { UserContext } from '../../contextApp/userContext';
 
 // const socket = io();
 // const socket = io("http://192.168.1.100:4004")
-const socket = io("http://10.1.5.217:4008")
+// const socket = io("http://10.1.5.217:4008")
+const socket = io("http://10.40.10.1:4008")   // conexão vpn
 
 
 export const Sliders = () => {
 
   const [isConnected, setIsConnected] = useState<boolean>(false);
-  const [nome, setNome] = useState<string>("");
-  const [goPage, setGoPage] = useState<boolean>(false)
 
   const { setCliente } = useContext(UserContext);
 
 
-  // useEffect(() => {
-  //   socket.on("ws-response", (res) => {
-  //     console.log("Cliente de ", res);
-  //     console.log(String(isConnected))
-  //   })
-  // }, [isConnected])
-
-
-  // useEffect(() => {
-  //   setInterval(() => {
-  //     setGoPage(prev => !prev);
-  //   }, 2000)
-  // }, [])
-
   socket.on("ws-response", (res) => {
     console.log("Cliente de ", res);
-    // console.log(socket.connected);
-    // console.log(String(isConnected));
+
     setIsConnected(res.flag);
     setCliente(res.cliente)
     
-    // socket.emit("ws-disconnect", {flag: true});
     if (res.flag) {
       socket.close();
     } else {
@@ -47,49 +30,13 @@ export const Sliders = () => {
     } 
   })
 
-  // socket.emit("ws-disconnect", {flag: true});
-
   return (
     <div>
-      {/* {isConnected
-        && <Navigate to="/nps" replace={true} />
-      } */}
-
       {isConnected
         && <Navigate to="/nps" replace={true} />
       }
 
       <h1>Sliders</h1>
-      {/* {goPage && <Navigate to="/nps" replace={true} />} */}
-      {/* <button
-        onClick={() => setInterval(() => setGoPage(prev => !prev), 200)}
-      >Avançar</button> */}
-
     </div>
   )
 }
-
-
-
-
-
-
-// return (
-
-//   <div>
-
-//     <p>Sliders</p>
-//     {goPage && <Navigate to="/nps" replace={true} />}
-//     <button
-//       onClick={() => setInterval(() => setGoPage(prev => !prev), 200)}
-//     >Avançar</button>
-//     <>
-//       {
-//         socket.on("ws-response", (res) => {
-//           console.log("Cliente de ", res);
-//           console.log(String(isConnected))
-//         })
-//       }
-//     </>
-//   </div>
-// )
